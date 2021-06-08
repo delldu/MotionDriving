014a47c31436caeec43b330942e2f41d17cc59bf
Dell Du <18588220928@163.com>
2021-06-08 18:03:27 +0800

    Add test shortcut, images, videos

---
+++ demo.sh
@@ -0,0 +1,12 @@
+python demo.py  \
+	--config config/vox256.yaml \
+	--source_image $1 \
+	--driving_video $2 \
+	--checkpoint checkpoints/vox256.pth
+
+
+# python demo.py  \
+# 	--config config/ted384.yaml \
+# 	--source_image $1 \
+# 	--driving_video $2 \
+# 	--checkpoint checkpoints/ted384.pth
