6cbd14509c115389bf0cf01fc06264f07f8b87a7
Dell Du <18588220928@163.com>
2021-06-17 16:01:00 +0800

    Update

--- demo.sh
+++ demo.sh
@@ -2,9 +2,8 @@ python demo.py  \
 	--config config/vox256.yaml \
 	--source_image $1 \
 	--driving_video $2 \
+	--img_shape=256,256 \
 	--checkpoint checkpoints/new_vox256.pth
-
-# --img_shape=512,512
 
 # python demo.py  \
 # 	--config config/ted384.yaml \
