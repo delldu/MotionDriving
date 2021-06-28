3c23b9ba3865038fde0481a5f160db0e2e113eac
Dell Du <18588220928@163.com>
2021-06-29 01:46:44 +0800

    Update

--- project/onnx_model.py
+++ project/onnx_model.py
@@ -60,7 +60,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-e', '--export', help="export onnx model", action='store_true')
     parser.add_argument('-v', '--verify', help="verify onnx model", action='store_true')
-    parser.add_argument('-p', '--predict', help="predict with onnx model", action='store_true')
     parser.add_argument('-o', '--output', type=str, default="output", help="output folder")
 
     args = parser.parse_args()
@@ -76,25 +75,27 @@ if __name__ == '__main__':
     # ************************************************************************************/
     #
 
-    dummy_input = torch.randn(1, 1, 1, 512)
-    onnx_file_name = "{}/model.onnx".format(args.output)
+    dummy_source = torch.randn(1, 3, 256, 256)
+    dummy_driving = torch.randn(1, 3, 256, 256)
+
+    checkpoint = "models/image_motion.pth"
+    onnx_file_name = "{}/image_motion.onnx".format(args.output)
 
     def export_onnx():
         """Export onnx model."""
 
         # 1. Create and load model.
-        torch_model = get_model()
-        torch_model = torch_model.cuda()
-        torch_model.eval()
+        model = get_model(checkpoint)
+        # model = model.cuda()
+        model.eval()
 
         # 2. Model export
         print("Exporting onnx model to {}...".format(onnx_file_name))
 
-        input_names = ["input"]
+        input_names = ["source", "driving"]
         output_names = ["output"]
-        # dynamic_axes = {'input': {0: "batch"},'output': {0: "batch"}}
 
-        torch.onnx.export(torch_model, dummy_input, onnx_file_name,
+        torch.onnx.export(model, (dummy_source, dummy_driving), onnx_file_name,
                           input_names=input_names,
                           output_names=output_names,
                           verbose=True,
@@ -103,19 +104,19 @@ if __name__ == '__main__':
                           export_params=True)
 
         # 3. Optimize model
-        # print('Checking model ...')
-        # onnx_model = onnx.load(onnx_file_name)
-        # onnx.checker.check_model(onnx_model)
+        print('Checking model ...')
+        onnx_model = onnx.load(onnx_file_name)
+        onnx.checker.check_model(onnx_model)
         # https://github.com/onnx/optimizer
 
         # 4. Visual model
-        # python -c "import netron; netron.start('output/model.onnx')"
+        # python -c "import netron; netron.start('output/image_motion.onnx')"
 
     def verify_onnx():
         """Verify onnx model."""
 
-        torch_model = get_model()
-        torch_model.eval()
+        model = get_model(checkpoint)
+        model.eval()
 
         onnxruntime_engine = onnx_load(onnx_file_name)
 
@@ -123,48 +124,15 @@ if __name__ == '__main__':
             return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
 
         with torch.no_grad():
-            torch_output = torch_model(dummy_input)
+            torch_output = model(dummy_source, dummy_driving)
 
-        onnxruntime_inputs = {onnxruntime_engine.get_inputs()[0].name: to_numpy(dummy_input)}
+        onnxruntime_inputs = {onnxruntime_engine.get_inputs()[0].name: to_numpy(dummy_source),
+            onnxruntime_engine.get_inputs()[1].name: to_numpy(dummy_driving),
+        }
         onnxruntime_outputs = onnxruntime_engine.run(None, onnxruntime_inputs)
 
         np.testing.assert_allclose(to_numpy(torch_output), onnxruntime_outputs[0], rtol=1e-03, atol=1e-03)
         print("Onnx model {} has been tested with ONNXRuntime, result sounds good !".format(onnx_file_name))
-
-    def onnx_predict():
-        print("Onnx predicting ...")
-
-        #
-        # /************************************************************************************
-        # ***
-        # ***    MS: Define Input/Output File
-        # ***
-        # ************************************************************************************/
-        #
-
-        input_image_file = "lena.png"
-        output_image_file = "{}/{}".format(args.output, input_image_file)
-
-        # /************************************************************************************
-        # ***
-        # ***    MS: Normal Predict Flow
-        # ***
-        # ************************************************************************************/
-        image = Image.open(input_image_file).convert("RGB")
-        onnx_model = onnx_load(onnx_file_name)
-
-        start_time = time.time()
-        totensor = transforms.ToTensor()
-        toimage = transforms.ToPILImage()
-
-        input_image_tensor = totensor(input_image).unsqueeze(0)
-        output_image_tensor = onnx_forward(onnx_model, input_image_tensor)
-        output_image = toimage(output_image_tensor.squeeze(0))
-
-        spend_time = time.time() - start_time
-        print("Spend time: {:.2f} seconds".format(spend_time))
-
-        output_image.save(output_image_file)
 
     #
     # /************************************************************************************
@@ -179,6 +147,3 @@ if __name__ == '__main__':
 
     if args.verify:
         verify_onnx()
-
-    if args.predict:
-        onnx_predict()
