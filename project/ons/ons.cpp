0d05415d463ca4a9a1f2fb2e8d23efee3e46b8f3
delldu <18588220928@163.com>
2021-07-26 21:33:28 +0800

    Add test

--- project/ons/ons.cpp
+++ project/ons/ons.cpp
@@ -1,14 +1,13 @@
-#include <torch/extension.h>
 #include <iostream>
+#include <torch/extension.h>
 
+torch::Tensor leftsv(const torch::Tensor &self) {
+  torch::Tensor U, S, V;
+  std::tie(U, S, V) = self.svd();
+  torch::Tensor d =
+      S.pow(0.5).diag_embed(/*offset=*/0, /*dim1=*/-2, /*dim2=*/-1);
 
-torch::Tensor leftsv(const torch::Tensor &self)
-{
-    torch::Tensor U, S, V;
-    std::tie(U, S, V) = self.svd();
-    torch::Tensor d = S.pow(0.5).diag_embed(/*offset=*/0, /*dim1=*/-2, /*dim2=*/-1);
-    
-    return torch::matmul(U, d);
+  return torch::matmul(U, d);
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
